e8b142bfc962362f7d0cb8d0bfe3f1d842b90db7
Petteri <Buggis123buggis@gmail.com>
2018-01-21 09:52:33 +0100

    Initial Commit

---
+++ Labb2_Petteri/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labb2_Petteri
+//
+//  Created by lösen är 0000 on 2018-01-21.
+//  Copyright © 2018 Petteri Tuononen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
